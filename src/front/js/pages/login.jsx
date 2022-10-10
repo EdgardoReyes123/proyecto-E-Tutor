85be27a9a438ef78f1b7f958b47aa975cb0e5665
Maryvi <mcamachog@hotmail.com>
2022-10-10 21:03:36 +0000

    navegando login y register

--- src/front/js/pages/login.jsx
+++ src/front/js/pages/login.jsx
@@ -1,14 +1,4 @@
 import React, { useContext } from "react";
-// import {
-//   MDBBtn,
-//   MDBContainer,
-//   MDBRow,
-//   MDBCol,
-//   MDBCard,
-//   MDBCardBody,
-//   MDBInput,
-//   MDBIcon,
-// } from "mdb-react-ui-kit";
 import { Context } from "../store/appContext";
 import rigoImageUrl from "../../img/rigo-baby.jpg";
 import "../../styles/home.css";
@@ -17,128 +7,48 @@ export const Login = () => {
   const { store, actions } = useContext(Context);
 
   return (
-    <div>
-      {/* <MDBContainer fluid>
-        <MDBRow className="d-flex justify-content-center align-items-center h-100">
-          <MDBCol col="12">
-            <MDBCard
-              className="bg-dark text-white my-5 mx-auto"
-              style={{ borderRadius: "1rem", maxWidth: "400px" }}
-            >
-              <MDBCardBody className="p-5 d-flex flex-column align-items-center mx-auto w-100">
-                <h2 className="fw-bold mb-2 text-uppercase">Login</h2>
-                <p className="text-white-50 mb-5">
-                  Please enter your login and password!
-                </p>
-
-                <MDBInput
-                  wrapperClass="mb-4 mx-5 w-100"
-                  labelClass="text-white"
-                  label="Email address"
-                  id="formControlLg"
-                  type="email"
-                  size="lg"
-                />
-                <MDBInput
-                  wrapperClass="mb-4 mx-5 w-100"
-                  labelClass="text-white"
-                  label="Password"
-                  id="formControlLg"
-                  type="password"
-                  size="lg"
-                />
-
-                <p className="small mb-3 pb-lg-2">
-                  <a class="text-white-50" href="#!">
-                    Forgot password?
-                  </a>
-                </p>
-                <MDBBtn outline className="mx-2 px-5" color="white" size="lg">
-                  Login
-                </MDBBtn>
-
-                <div className="d-flex flex-row mt-3 mb-5">
-                  <MDBBtn
-                    tag="a"
-                    color="none"
-                    className="m-3"
-                    style={{ color: "white" }}
-                  >
-                    <MDBIcon fab icon="facebook-f" size="lg" />
-                  </MDBBtn>
-
-                  <MDBBtn
-                    tag="a"
-                    color="none"
-                    className="m-3"
-                    style={{ color: "white" }}
-                  >
-                    <MDBIcon fab icon="twitter" size="lg" />
-                  </MDBBtn>
-
-                  <MDBBtn
-                    tag="a"
-                    color="none"
-                    className="m-3"
-                    style={{ color: "white" }}
-                  >
-                    <MDBIcon fab icon="google" size="lg" />
-                  </MDBBtn>
-                </div>
-
-                <div>
-                  <p className="mb-0">
-                    Don't have an account?{" "}
-                    <a href="#!" class="text-white-50 fw-bold">
-                      Sign Up
-                    </a>
-                  </p>
-                </div>
-              </MDBCardBody>
-            </MDBCard>
-          </MDBCol>
-        </MDBRow>
-      </MDBContainer> */}
-
-      <div className="login container-fluid">
+    <div className="d-flex justify-content-center">
+      <div className="">
         <div className="heading">
           <h2>Sign in</h2>
-          <form action="#">
-            <div className="input-group input-group-lg">
-              <span className="input-group-addon">
-                <i className="fa fa-user"></i>
-              </span>
-              <input
-                type="text"
-                className="form-control"
-                placeholder="Username or email"
-              />
-            </div>
+          <div className="">
+            <form action="#">
+              <div className="input-group input-group-lg">
+                <span className="input-group-addon">
+                  <i className="fa fa-user"></i>
+                </span>
+                <input
+                  type="text"
+                  className="form-control"
+                  placeholder="Username or email"
+                />
+              </div>
 
-            <div className="input-group input-group-lg">
-              <span className="input-group-addon">
-                <i className="fa fa-lock"></i>
-              </span>
-              <input
-                type="password"
-                className="form-control"
-                placeholder="Password"
-              />
-            </div>
+              <div className="input-group input-group-lg">
+                <span className="input-group-addon">
+                  <i className="fa fa-lock"></i>
+                </span>
+                <input
+                  type="password"
+                  className="form-control"
+                  placeholder="Password"
+                />
+              </div>
 
-            <button type="submit" className="float">
-              Login
-            </button>
-          </form>
+              <button type="submit" className="btn btn-primary">
+                Login
+              </button>
+            </form>
+          </div>
         </div>
       </div>
 
-      <div className="text-center mt-5">
+      {/* <div className="text-center mt-5">
         <form>
           <input type="text" placeholder="usuario" />
           <input type="password" placeholder="password" />
         </form>
-      </div>
+      </div> */}
     </div>
   );
 };
--- src/front/js/pages/register.jsx
+++ src/front/js/pages/register.jsx
@@ -7,16 +7,58 @@ export const Register = () => {
   const { store, actions } = useContext(Context);
 
   return (
-    <div className="text-center mt-5">
-      <h1>Hello Rigo!! REGISTER</h1>
-      <p>
-        <img src={rigoImageUrl} />
-      </p>
-      <div className="alert alert-info">
-        {store.message ||
-          "Loading message from the backend (make sure your python backend is running)..."}
+    <div className="d-flex justify-content-center">
+      <div className="">
+        <div className="heading">
+          <h2>Register</h2>
+          <div className="">
+            <form action="#">
+              <div className="input-group input-group-lg">
+                <span className="input-group-addon">
+                  <i className="fa fa-user"></i>
+                </span>
+                <input
+                  type="text"
+                  className="form-control"
+                  placeholder="Username or email"
+                />
+              </div>
+              <div className="input-group input-group-lg">
+                <span className="input-group-addon">
+                  <i className="fa fa-lock"></i>
+                </span>
+                <input
+                  type="password"
+                  className="form-control"
+                  placeholder="Password"
+                />
+              </div>
+
+              <div className="input-group input-group-lg">
+                <span className="input-group-addon">
+                  <i className="fa fa-lock"></i>
+                </span>
+                <input
+                  type="password"
+                  className="form-control"
+                  placeholder="Repeat Password"
+                />
+              </div>
+
+              <button type="submit" className="btn btn-primary">
+                Register
+              </button>
+            </form>
+          </div>
+        </div>
       </div>
-      <h1>Cambios para hacer el register</h1>
+
+      {/* <div className="text-center mt-5">
+        <form>
+          <input type="text" placeholder="usuario" />
+          <input type="password" placeholder="password" />
+        </form>
+      </div> */}
     </div>
   );
 };
