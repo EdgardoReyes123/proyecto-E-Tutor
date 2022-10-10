cb933c4a34aaad6d7dc50d53b84fac0fc7cc040d
joselyngm11 <joselyngm.47@gmail.com>
2022-10-10 21:58:50 +0000

    Carousel

--- src/front/js/component/FirstHeading.jsx
+++ src/front/js/component/FirstHeading.jsx
@@ -1,32 +1,60 @@
-import React from "react";
 import { Link } from "react-router-dom";
 import Container from "react-bootstrap/Container";
-import Nav from "react-bootstrap/Nav";
-import Navbar from "react-bootstrap/Navbar";
+import React, { useContext, useEffect, useState } from "react";
 
-export const FirstHeading = () => {
+const FirstHeading = () => {
+  const data = ["ITEM 1", "ITEM 2", "ITEM 3"];
+  const [currentIndex, setCurrentIndex] = useState(0);
+  const carouselInfiniteScroll = () => {
+    if (currentIndex === data.length - 1) {
+      return setCurrentIndex(0);
+    }
+    return setCurrentIndex(currentIndex + 1);
+  };
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      carouselInfiniteScroll();
+    }, 3000);
+    //clean up function
+    return () => clearInterval(interval);
+  });
+
   return (
     <div id="About" className="container border-top">
-        <div className="row p-5 mt-5">
-                
+      <div className="carousel row p-5 mt-5">
+        {data.map((item, index) => {
+          return (
+            <h1
+              className="carouselItem"
+              style={{ transform: `translate(-${currentIndex * 100}%)` }}
+              key={index}
+            >
+              {item}
+            </h1>
+          );
+        })}
+      </div>
+      <div className="row p-5 mt-5">
+        <div className="headingText col-md-12">
+          <h3 style={{ fontSize: "37px" }}>
+            The best way to LEARN <span>Will Catch Your Eye</span>
+          </h3>
+          <p style={{ fontSize: "18px" }}>
+            {" "}
+            Donec ullamcorper nulla non metus auctor fringilla. Vestibulum id
+            ligula porta felis euismod semper. Praesent commodo cursus magna,
+            vel scelerisque nisl consectetur. Fusce dapibus, tellus ac cursus
+            commodo.
+          </p>
         </div>
-        <div className="row p-5 mt-5">
-                <div className="headingText col-md-12">
-                    <h3 style={{fontSize: "37px"}}>
-                        The best way to LEARN <span>Will Catch Your Eye</span>
-                    </h3>
-                    <p style={{fontSize: "18px"}}>  Donec ullamcorper nulla non metus auctor fringilla. Vestibulum
-                        id ligula porta felis euismod semper. Praesent commodo cursus magna,
-                        vel scelerisque nisl consectetur. Fusce dapibus, tellus ac cursus commodo.
-                    </p>
-                </div>
-                <div className="col-12 text-end">
-                    <i className="fas fa-play-circle playIcon"></i>
-                    <span className="linkToVideo">Will Catch Your Eye</span>
-                </div>
+        <div className="col-12 text-end">
+          <i className="fas fa-play-circle playIcon"></i>
+          <span className="linkToVideo">Will Catch Your Eye</span>
         </div>
+      </div>
     </div>
-    );
+  );
 };
-    
+
 export default FirstHeading;
