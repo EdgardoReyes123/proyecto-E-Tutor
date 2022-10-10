44a5ae19b669506f5f917a682f0910be6658b204
Maryvi <mcamachog@hotmail.com>
2022-10-10 16:37:41 +0000

    primera navegacion

--- src/front/js/component/Navbar.jsx
+++ src/front/js/component/Navbar.jsx
@@ -10,9 +10,9 @@ export const NavbarP = () => {
       <Container>
         <Navbar.Brand href="/">E-Tutor</Navbar.Brand>
         <Nav className="me-auto">
-          <Nav.Link href="#home">Home</Nav.Link>
-          <Nav.Link href="#features">Class</Nav.Link>
-          <Nav.Link href="#pricing">Pricing</Nav.Link>
+          <Nav.Link href="/">Home</Nav.Link>
+          <Nav.Link href="/login">Login</Nav.Link>
+          <Nav.Link href="/register">Register</Nav.Link>
         </Nav>
       </Container>
     </Navbar>
