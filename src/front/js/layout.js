30eb9f1a3898c8f4fefd55c9367f5c52e1e7bec7
Edgardo Reyes <edgardoreyesbelmonte@gmail.com>
2022-10-11 20:22:04 +0000

    o

--- src/front/js/layout.js
+++ src/front/js/layout.js
@@ -9,9 +9,10 @@ import injectContext from "./store/appContext";
 
 import { Navbar } from "./component/navbar";
 import { Footer } from "./component/footer";
+import { Profile } from "./pages/profile";
 
 //create your first component
-const Layout = () => {
+function Layout() {
   //the basename is used when your project is published in a subdirectory and not in the root of the domain
   // you can set the basename on the .env file located at the root of this project, E.g: BASENAME=/react-hello-webapp/
   const basename = process.env.BASENAME || "";
@@ -25,6 +26,7 @@ const Layout = () => {
             <Route element={<Home />} path="/" />
             <Route element={<Demo />} path="/demo" />
             <Route element={<Single />} path="/single/:theid" />
+            <Route element={<Profile />} path="/profile" />
             <Route element={<h1>Not found!</h1>} />
           </Routes>
           <Footer />
@@ -32,6 +34,6 @@ const Layout = () => {
       </BrowserRouter>
     </div>
   );
-};
+}
 
 export default injectContext(Layout);
