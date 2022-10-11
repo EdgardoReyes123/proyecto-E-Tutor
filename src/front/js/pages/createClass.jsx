69070e37ded9d67eabb57dceb1b8d735dbd2bc8d
Maryvi <mcamachog@hotmail.com>
2022-10-11 03:33:08 +0000

    crear Clase version 1

--- src/front/js/pages/createClass.jsx
+++ src/front/js/pages/createClass.jsx
@@ -2,6 +2,10 @@ import React, { useContext } from "react";
 import { Context } from "../store/appContext";
 import rigoImageUrl from "../../img/rigo-baby.jpg";
 import "../../styles/home.css";
+import Col from "react-bootstrap/Col";
+import Form from "react-bootstrap/Form";
+import Row from "react-bootstrap/Row";
+import Stack from "react-bootstrap/Stack";
 
 export const CreateClass = () => {
   const { store, actions } = useContext(Context);
@@ -17,26 +21,72 @@ export const CreateClass = () => {
   return (
     <div>
       <div>
-        <h1>
-          <i class="fa-regular fa-star"></i>Event
-        </h1>
+        <span>
+          <i className="fa-regular fa-star"></i>
+        </span>
+        <h1>Event</h1>
         <button>Close</button>
       </div>
       <div>
-        <form id="myForm" action="">
-          <label htmlFor="eventName">Event Name</label>
-          <input id="eventName" type="text" />
-          <label htmlFor="startDate">Start Date</label>
-          <input id="startDate" type="time" />
-          <label htmlFor="endDate">End Date</label>
-          <input id="endDate" type="time" />
-          <label htmlFor="allDay">All-day</label>
-          <input id="allDay" type="checkbox" />
-          <label htmlFor="calendar">Calendar</label>
-          <input id="calendar" type="text" />
-          <label htmlFor="details">Details</label>
-          <textarea name="details" id="details" cols="30" rows="10"></textarea>
-        </form>
+        <Form id="myForm" action="">
+          <Form.Group as={Row} className="mb-2 bg-light p-2 mt-3">
+            <Col sm={3}></Col>
+
+            <Form.Label column sm={1} className="mb-2 bg-light">
+              Event Name
+            </Form.Label>
+            <Col sm={5}>
+              <Form.Control id="eventName" type="text" />
+            </Col>
+
+            <Col sm={3}></Col>
+
+            <Col sm={3}></Col>
+            <Form.Label column sm={1} className="mb-2">
+              Start Date
+            </Form.Label>
+            <Col sm={5}>
+              <Form.Control id="startDate" type="datetime-local" />
+            </Col>
+            <Col sm={3}></Col>
+
+            <Col sm={3}></Col>
+            <Form.Label column sm={1} className="mb-2">
+              End Date
+            </Form.Label>
+            <Col sm={5}>
+              <Form.Control id="endDate" type="datetime-local" />
+            </Col>
+            <Col sm={3}></Col>
+
+            <Col sm={3}></Col>
+            <Form.Label column sm={1} className="mb-2">
+              All-day
+            </Form.Label>
+            <Col sm={5}>
+              <Form.Check id="allDay" type="checkbox" />
+            </Col>
+            <Col sm={3}></Col>
+
+            <Col sm={3}></Col>
+            <Form.Label column sm={1} className="mb-2">
+              Calendar
+            </Form.Label>
+            <Col sm={5}>
+              <Form.Control id="calendar" type="text" />
+            </Col>
+            <Col sm={3}></Col>
+
+            <Col sm={3}></Col>
+            <Form.Label column sm={1} className="mb-2">
+              Details
+            </Form.Label>
+            <Col sm={5}>
+              <Form.Control as="textarea" rows={3} />
+            </Col>
+            <Col sm={3}></Col>
+          </Form.Group>
+        </Form>
         <button form="myForm" type="submit">
           Save
         </button>
