353daa8a8a3724323a3d223cabfca6a205dcca2b
Iván Carpio <ivancarpio50@gmail.com>
2022-10-14 21:34:20 +0000

    Primer commit de profile

--- src/front/js/pages/profile.jsx
+++ src/front/js/pages/profile.jsx
@@ -1,42 +1,90 @@
-import React from "react";
+import React, { useState } from "react";
 import Button from "react-bootstrap/Button";
-//import { Container } from "react-bootstrap/Container";
-//import { Row } from "react-bootstrap/Row";
-//import { Col } from "react-bootstrap/Col";
+import { ListGroup } from "react-bootstrap";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import "../../styles/profile.css";
 
 export function Profile() {
+  const [post, setPost] = useState([]);
+  const [inputPost, setInputPost] = useState("");
+
+  const handleAdd = (e) => {
+    if (e.key == "Enter") {
+      if (inputPost != "") {
+        setPost([...post, inputPost]);
+        setInputPost("");
+      } else {
+        alert("No puede estar vacio");
+      }
+    }
+  };
+
   return (
     <div className="Container" fluid="md">
-      <div className="Row">
-        <div className="Col1">
-          <div className="Portada">
-            <img src="https://i.picsum.photos/id/582/500/150.jpg?hmac=LlpH0hVyiXUveq_1ZxIN_r2YOa2_XG7yXUauK7P63wc"></img>
-          </div>
-          <div className="imgperfil">
-            {" "}
-            <img src="https://i.picsum.photos/id/276/100/100.jpg?hmac=UaT7sojlOY8hIbIHpc9DEwjr3VSLkqfFGG-imYhlolw"></img>
-          </div>
-          <div>
-            <h5>4geeeks Academy Venezuela</h5>
-            <p>
-              Somos una academia de programacion web. Nuestra proxima cohorte
-              online inicia el 17 de octubre
-            </p>
-            <div variant="primary" type="submit">
-              <Button className="Button" variant="primary">
-                Following
-              </Button>
-              <Button className="Button" variant="primary">
-                Visit Website
-              </Button>
-              <Button className="Button" variant="primary">
-                More
-              </Button>
+      <Row className="Row">
+        <Col sm={10}>
+          <div className="hero">
+            <div className="Portada">
+              <img src="https://i.picsum.photos/id/324/1180/300.jpg?hmac=IzsPd2zvvUbzOYRufGgA56ZnVoiw_QnjFSx2rDUItQE"></img>
+            </div>
+            <div className="imgperfil">
+              <img src="https://i.picsum.photos/id/276/100/100.jpg?hmac=UaT7sojlOY8hIbIHpc9DEwjr3VSLkqfFGG-imYhlolw"></img>
+            </div>
+            <div className="data">
+              <h5>4Geeeks Academy Venezuela</h5>
+              <p>
+                Somos una academia de programacion web. Nuestra proxima cohorte
+                online inicia el 17 de octubre
+              </p>
+              <div>
+                <Button className="Button follow" variant="primary">
+                  Following
+                </Button>
+                <Button className="Button" variant="primary">
+                  Visit Website
+                </Button>
+                <Button className="Button" variant="primary">
+                  More
+                </Button>
+              </div>
             </div>
           </div>
-        </div>
-        <div className="Col2">2 of 2</div>
-      </div>
+        </Col>
+
+        <input
+          className="bar"
+          onChange={(e) => setInputPost(e.target.value)}
+          value={inputPost}
+          onKeyDown={handleAdd}
+          type="text"
+          placeholder="Add new post..."
+        />
+
+        <ListGroup className="col-sm-8">
+          {post.map((post, index) => {
+            return (
+              <div className="post">
+                <ListGroup.Item key={index}>
+                  <div className="postInfo">
+                    <img src="https://i.picsum.photos/id/402/50/50.jpg?hmac=TKqsv4y6IYI-OkMqcmihYayIT8u9SsQa4OO7wRnmW8s"></img>
+                    <p className="title">4Geeks Academy Venezuela</p>
+                  </div>
+                  <div className="postData">
+                    <p>{post}</p>
+                  </div>
+                  {/*<div className="postImg">
+                      <img
+                        src="https://i.picsum.photos/id/907/300/500.jpg?hmac=5JhmKFTFA0Ob7w_lnuzpmaHOyLPFUDzNW0ZzzpZzwL8"
+                        alt=""
+                      />
+                    </div>*/}
+                </ListGroup.Item>
+              </div>
+            );
+          })}
+        </ListGroup>
+      </Row>
     </div>
   );
 }
