3a7f2a0344961bddaacd5b0cc958b9c9476d86d3
Maryvi <mcamachog@hotmail.com>
2022-10-14 22:43:18 +0000

    buscar clase inicio

--- src/front/js/layout.js
+++ src/front/js/layout.js
@@ -12,6 +12,7 @@ import { Navbar } from "./component/navbar";
 import { Footer } from "./component/footer";
 import { Register } from "./pages/register";
 import { CreateClass } from "./pages/createClass";
+import { SearchClass } from "./pages/searchClass";
 
 //create your first component
 const Layout = () => {
@@ -29,6 +30,7 @@ const Layout = () => {
             <Route element={<Register />} path="/register" />
             <Route element={<Login />} path="/login" />
             <Route element={<CreateClass />} path="/createClass" />
+            <Route element={<SearchClass />} path="/searchClass" />
             <Route element={<Single />} path="/single/:theid" />
             <Route element={<h1>Not found!</h1>} />
           </Routes>
--- src/front/js/pages/searchClass.jsx
+++ src/front/js/pages/searchClass.jsx
@@ -5,10 +5,10 @@ import Col from "react-bootstrap/Col";
 import Form from "react-bootstrap/Form";
 import Row from "react-bootstrap/Row";
 import "../../styles/home.css";
+import Button from "react-bootstrap/Button";
+import Card from "react-bootstrap/Card";
 
 export const SearchClass = () => {
-  const { store, actions } = useContext(Context);
-
   return (
     <>
       <div>
@@ -16,64 +16,59 @@ export const SearchClass = () => {
           <span className="p-2 center-label">
             <i className="fa fa-star "></i>
           </span>
-          <h1 className="p-2 flex-grow-1">Login</h1>
-          <button className="btn btn-primary p-2 m-2">Close</button>
+          <h1 className="p-2 flex-grow-1">Clases disponibles</h1>
         </div>
-        <div>
-          <Form id="myForm" action="">
-            <Form.Group as={Row} className="mb-2 bg-light p-2 mt-3">
-              <Col sm={3}></Col>
 
-              <Form.Label column sm={2} className="mb-2 bg-light">
-                Usuario
-              </Form.Label>
-              <Col sm={3}>
-                <Form.Control id="user" type="text" />
-              </Col>
-              <Col sm={4}></Col>
+        <Form id="myForm" action="" className="d-flex justify-content-center">
+          <Form.Control
+            id="palabraClave"
+            type="text"
+            placeholder="Palabra Clave"
+          />
+          <Form.Label column sm={2} className="mb-2 bg-light">
+            Fecha
+          </Form.Label>
+          <Form.Control id="fecha" type="date" />
+          <Form.Select aria-label="Default select example">
+            <option>Tutor</option>
+            <option value="1">One</option>
+            <option value="2">Two</option>
+            <option value="3">Three</option>
+          </Form.Select>
+        </Form>
 
-              <Col sm={3}></Col>
-              <Form.Label column sm={2} className="mb-2">
-                Password
-              </Form.Label>
-              <Col sm={3}>
-                <Form.Control id="password" type="password" />
-              </Col>
-              <Col sm={4}></Col>
-
-              <Col sm={3}></Col>
-              <Form.Label column sm={2} className="mb-2">
-                Perfil
-              </Form.Label>
-
-              <Col sm={1}>
-                <Form.Check
-                  label="Tutor"
-                  type="radio"
-                  name="profile"
-                  value="tutor"
-                />
-              </Col>
-              <Col sm={1}>
-                <Form.Check
-                  label="Estudiante"
-                  type="radio"
-                  name="profile"
-                  value="student"
-                />
-              </Col>
-              <Col sm={5}></Col>
-            </Form.Group>
-          </Form>
-          <div className="d-flex justify-content-center">
-            <button
-              className="btn btn-primary p-2 m-2"
-              form="myForm"
-              type="submit"
-            >
-              Login
-            </button>
-          </div>
+        <div className="d-flex justify-content-center">
+          <button
+            className="btn btn-primary p-2 m-2"
+            form="myForm"
+            type="submit"
+          >
+            Buscar
+          </button>
+        </div>
+        <div className="d-flex">
+          <Card style={{ width: "18rem" }}>
+            <Card.Img variant="top" src="holder.js/100px180" />
+            <Card.Body>
+              <Card.Title>Card Title</Card.Title>
+              <Card.Text>
+                Some quick example text to build on the card title and make up
+                the bulk of the card's content.
+              </Card.Text>
+              <Button variant="primary">Go somewhere</Button>
+            </Card.Body>
+          </Card>
+          <Card style={{ width: "18rem" }}>
+            <Card.Img variant="top" src="holder.js/100px180" />
+            <Card.Body>
+              <Card.Title>Card Title</Card.Title>
+              <Card.Text>
+                Some quick example text to build on the card title and make up
+                the bulk of the card's content.
+              </Card.Text>
+              <Button variant="primary">Go somewhere</Button>
+            </Card.Body>
+          </Card>
         </div>
       </div>
     </>
