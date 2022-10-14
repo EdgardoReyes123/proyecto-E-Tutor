8a31945c42f727b8a92801f11cfec8a5137a78a1
joselyngm11 <joselyngm.47@gmail.com>
2022-10-14 03:21:08 +0000

    comments

--- src/front/js/component/UpcommingsClasses.jsx
+++ src/front/js/component/UpcommingsClasses.jsx
@@ -2,6 +2,7 @@ import { Link } from "react-router-dom";
 import Container from "react-bootstrap/Container";
 import Carousel from 'react-bootstrap/Carousel';
 import React, { useContext, useEffect, useState } from "react";
+import Image from "react-bootstrap/Image";
 import { Context } from "../store/appContext";
 import UpcommingClassesCard from "../component/UpcommingClassesCard.jsx";
 
@@ -47,8 +48,38 @@ const UpcommingsClasses = () => {
                         <h2 className="title">Comments</h2>
                     </div>
                     <div className="tarjeta-comments">
-                        <div className="comment-item"></div>
-                        <div className="comment-item"></div>
+                        <div className="comment-item">
+                            <div className="d-inline-flex">
+                                <Image src="https://www.mundopsicologos.com/site/article/60606/50895/las-10-cualidades-de-una-persona-mas-valoradas-0_ai1.jpg" 
+                                className="person rounded-circle mw-100" alt="p1"/>
+                                <div>
+                                    <h3 className="texto-title">
+                                        The Second Heading <span>Is Pretty Cool Too.</span>
+                                    </h3>
+                                    <p className="texto"> 
+                                        Donec ullamcorper nulla non metus auctor fringilla. Vestibulum
+                                        id ligula porta felis euismod semper. Praesent commodo cursus magna,
+                                        vel scelerisque nisl consectetur. Fusce dapibus, tellus ac cursus commodo.
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="comment-item2">
+                            <div className="d-inline-flex">
+                                <div>
+                                    <h3 className="texto-title">
+                                        The Second Heading <span>Is Pretty Cool Too.</span>
+                                    </h3>
+                                    <p className="texto"> 
+                                        Donec ullamcorper nulla non metus auctor fringilla. Vestibulum
+                                        id ligula porta felis euismod semper. Praesent commodo cursus magna,
+                                        vel scelerisque nisl consectetur.
+                                    </p>
+                                </div>
+                                <Image src="https://cdn.create.vista.com/api/media/medium/329263782/stock-photo-handsome-smiling-man-taking-selfie?token=" 
+                                className="person rounded-circle mw-100" alt="p1"/>
+                            </div>
+                        </div>
                     </div>
                 </div>
         </div>
