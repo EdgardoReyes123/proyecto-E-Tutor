a1b16c141a39163b7e9c0ee405f9f32c98bcc27e
joselyngm11 <joselyngm.47@gmail.com>
2022-10-26 02:40:40 +0000

    Login background and button

--- src/front/js/pages/login.jsx
+++ src/front/js/pages/login.jsx
@@ -5,21 +5,22 @@ import Col from "react-bootstrap/Col";
 import Form from "react-bootstrap/Form";
 import Row from "react-bootstrap/Row";
 import "../../styles/home.css";
+import FooterE from "../component/footer.js";
 
 export const Login = () => {
   const { store, actions } = useContext(Context);
 
   return (
-    <>
+    // <>
       <div className="container-fluid fondo">
-        <div className="d-flex">
+        {/* <div className="d-flex">
           <span className="p-2 center-label">
             <i className="fa fa-star "></i>
           </span>
           <h1 className="p-2 flex-grow-1">Login</h1>
           <button className="btn btn-primary p-2 m-2">Close</button>
-        </div>
-        <div>
+        </div> */}
+        <div className="row loginForm">
           <Form id="myForm" action="">
             <Form.Group as={Row} className="mb-2  p-2 mt-3">
               <Col sm={3}></Col>
@@ -67,7 +68,7 @@ export const Login = () => {
           </Form>
           <div className="d-flex justify-content-center">
             <button
-              className="btn btn-primary p-2 m-2"
+              className="loginButton btn btn-outline-success p-2 m-2"
               form="myForm"
               type="submit"
             >
@@ -76,6 +77,6 @@ export const Login = () => {
           </div>
         </div>
       </div>
-    </>
+    // </>
   );
 };
